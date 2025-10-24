0598cded1dfbc8da177176d5f06c5f6150251b1f
Nico Zeisler <nzeisler28@students.stab.org>
2025-10-24 13:09:49 -0400

    Update script.js to add firebase boilerplate

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+import {getFirestore, collection, doc, getDocs, addDoc, updateDoc, query, where, deleteDoc} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCAOfNj92YHafyu2sAdYSSsAPf5RcxZ2wg",
+  authDomain: "ceramicsstudio-deb67.firebaseapp.com",
+  projectId: "ceramicsstudio-deb67",
+  storageBucket: "ceramicsstudio-deb67.firebasestorage.app",
+  messagingSenderId: "1089998700895",
+  appId: "1:1089998700895:web:03a77d724f88b03b8736ea",
+  measurementId: "G-Q1W9FR3Z8C"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export const addItem = async function(item) {
+  const itemtoDo = await document.getElementById("image").value;
+  const docRef = await addDoc(collection(db, "projects"), {
+    studentName: username,
+    image: image,
+    firings: firings
+  });
+}
+
+export const completeFiring = async function(id) {
+  // Email to notify student when complete
+  // Delete doc from fb
+}
