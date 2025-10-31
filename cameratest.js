80f581eca5534b826a96a86d675b96897397c4f5
WillElias374 <146851033+WillElias374@users.noreply.github.com>
2025-10-31 13:35:48 -0400

    cameratest.js

---
+++ cameratest.js
@@ -0,0 +1,25 @@
+
+const video = document.querySelector(".video")
+const cameraButton = document.querySelector(".camera")
+const canvas = document.querySelector(".canvas")
+
+
+
+navigator.mediaDevices.getUserMedia({video: true})
+.then(stream => {
+    video.srcObject = stream;
+    video.play();
+})
+
+cameraButton.addEventListener("click", () => {
+    canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height)
+    let image_data_url = canvas.toDataUrl("image/jpeg")
+
+
+    const downloadLink = document.createElement("a")
+    downloadLink.href = image_data_url
+    downloadLink.download = "selfie.jpeg"
+    document.body.appendChild(downloadLink)
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+})
