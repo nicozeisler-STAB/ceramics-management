da0d31e0721aba7d93bf79557a7d3a44608d0d54
WillElias374 <146851033+WillElias374@users.noreply.github.com>
2025-11-19 11:13:49 -0500

    Add image loading

--- admin.js
+++ admin.js
@@ -23,9 +23,10 @@ export const showItems = async function(firingType){
     box.className = 'info-box'
     box.innerHTML = `
       <div class="info-title">${info.studentName}</div>
-      <div class="img" src="${info.image}"></<div>
+      <canvas class="placeHolderCanvas"></<div>
       <div class="info-detail">${info.signature}</div>
     `
+    drawFileOnCanvas(dataURLtoFile(info.image, "image.png"), box.querySelector("canvas")) 
     const startFiringButton = document.createElement("button")
     startFiringButton.innerHTML = "Start Firing"
     startFiringButton.onclick = async function() {
@@ -54,9 +55,10 @@ export const showFirings = async function() {
     box.className = 'info-box'
     box.innerHTML = `
       <div class="info-title">${info.studentName}</div>
-      <div class="img" src="${info.image}"></<div>
+      <canvas class="placeHolderCanvas"></<div>
       <div class="info-detail">${info.signature}</div>
     `
+    drawFileOnCanvas(dataURLtoFile(info.image, "image.png"), box.querySelector("canvas")) 
     const completeButton = document.createElement("button")
     completeButton.innerHTML = "Mark Complete"
     completeButton.onclick = async function() {
@@ -77,9 +79,48 @@ export const showArtShow = async function() {
     box.className = "info-box"
     box.innerHTML = `
       <div class="info-title">${info.studentName}</div>
-      <div class="img" src="${info.image}"></<div>
+      <canvas class="placeHolderCanvas"></<div>
       <div class="info-detail">${info.signature}</div>
     `
+    drawFileOnCanvas(dataURLtoFile(info.image, "image.png"), box.querySelector("canvas")) 
   column.appendChild(box)
   });
+}
+
+function drawFileOnCanvas(file, canvas) {
+  const ctx = canvas.getContext('2d');
+  const reader = new FileReader(); // read file to data URL
+  reader.onload = () => {
+    const img = new Image();
+    img.onload = () => {
+      // set canvas size to fit image while limiting to max dimensions
+      const maxW = 200; // change as needed
+      const maxH = 150;
+      let w = img.naturalWidth;
+      let h = img.naturalHeight;
+      const scale = Math.min(1, maxW / w, maxH / h);
+      w = Math.round(w * scale);
+      h = Math.round(h * scale);
+      canvas.width = w;
+      canvas.height = h;
+      ctx.clearRect(0, 0, w, h);
+      ctx.drawImage(img, 0, 0, w, h); // draw image to canvas
+    };
+    img.src = reader.result; // data URL
+  }
+  reader.readAsDataURL(file);
+}
+
+function dataURLtoFile(dataurl, filename) {
+    var arr = dataurl.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n);
+
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+
+    return new File([u8arr], filename, { type: mime });
 }
