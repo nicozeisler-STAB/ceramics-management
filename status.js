439e2edf32fed0963ffee6759e4a5c7d147d8469
WillElias374 <146851033+WillElias374@users.noreply.github.com>
2026-02-11 11:27:43 -0500

    Add image handling and update status messages

--- status.js
+++ status.js
@@ -20,27 +20,57 @@ function userName() {
   div.textContent = "" + name;
 }
 
+function drawFileOnCanvas(file) {
+  const reader = new FileReader(); // read file to data URL
+  reader.onload = () => {
+    const img = new Image();
+    console.log(img);
+    img.src = reader.result; // data URL
+    img.style.width = "30vw";
+    img.style.height = "auto";
+    img.style.marginTop = "5%";
+    const center = document.getElementById("centerbox")
+    center.appendChild(img);
+  }
+  reader.readAsDataURL(file);
+}
+
+function dataURLtoFile(dataurl, filename) {
+    var arr = dataurl.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n);
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, { type: mime });
+    
+}
+
 export const getStatus = async function() {
   const email = sessionStorage.getItem("email")
   const firingType = sessionStorage.getItem("firingType")
   const snapshot = await getDocs(query(collection(db, firingType), where("email", "==", email)))
   snapshot.forEach((doc) => {
-    const status = doc.data().status;
+    const status = doc.data().status
     if (status == "unfired") {
-      const divVar2 = document.getElementById("centerbox");
-      divVar2.textContent = "Your piece has been submitted!"
-      
-      //document.body.style.backgroundColor = "red"
+      const divVar2 = document.getElementById("centerbox")
+      divVar2.textContent = "Your piece has been submitted."
     }
     if (status == "firing") {
-      const divVar2 = document.getElementById("centerbox");
-      divVar2.textContent = "Your piece is firing!";
-      //document.body.style.backgroundColor = "red"
+      const divVar2 = document.getElementById("centerbox")
+      divVar2.textContent = "Your piece is firing."
     }
     if (status == "fired") {
       document.body.style.backgroundColor = "green"
     }
-  });
+    const img = doc.data().image
+    drawFileOnCanvas(dataURLtoFile(img, "image.png")) ;
+   
+    
+  })
 }
-getStatus();
+
 userName();
+getStatus();
